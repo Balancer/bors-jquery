9c0fda88355571c4fbbced725334f4521e225545
Balancer <balancer@balancer.ru>
2017-02-26 08:52:39 +0300

    bower-asset support

--- jQuery.php
+++ jQuery.php
@@ -4,4 +4,25 @@ namespace B2;
 
 class jQuery extends \jquery
 {
+	static function load($link=NULL)
+	{
+		// If not defined nor css, nor version, then using latest CDN or bower-asset package
+		if(empty($link))
+		{
+			// If not installed bower-asset/jquery then using CDN
+			if(empty(\bors::$bower_asset_packages['bower-asset/jquery']))
+			{
+				\bors_use('https://code.jquery.com/jquery-3.1.1.min.js');
+				return;
+			}
+
+			// Package bower-asset/jquery installed, use them
+			$bower_asset_url = \B2\Cfg::get('bower-assets.base_url', '/bower-asset');
+			\bors_use($bower_asset_url.'/jquery/dist/jquery.min.js');
+			return;
+		}
+
+		// Use old loader
+		return parent::load();
+	}
 }
